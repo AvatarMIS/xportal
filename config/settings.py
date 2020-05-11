c5d48119af5cfd4eea671df0be667ecf13bc8f7a
AvatarMIS <37340508+AvatarMIS@users.noreply.github.com>
2020-05-11 15:27:10 +0100

    1

--- config/settings.py
+++ config/settings.py
@@ -81,11 +81,14 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'xPortal',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD' : 'Ameli@2010',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 # PASSWORDS
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
